6668fdd0be1f3b52e3221ed0652b8d021a010101
Thiago Krempser <tkrempser@gmail.com>
2022-02-14 19:14:31 -0300

    Register model admin classes

--- website/pos/admin.py
+++ website/pos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from pos.models import Customer, Product, Order
+
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
