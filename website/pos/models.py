cad57d53ea2834a6bb932bf24902203719cba3f9
Thiago Krempser <tkrempser@gmail.com>
2022-02-14 18:27:17 -0300

    Add models + Update class diagram

--- website/pos/models.py
+++ website/pos/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+
+class Product(models.Model):
+    comission = models.FloatField()
+    price = models.FloatField()
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(
+        Customer, on_delete=models.CASCADE, related_name='orders')
+    seller = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='orders')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+
+    class Meta:
+        unique_together = ('order', 'product')
